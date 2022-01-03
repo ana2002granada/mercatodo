85d4b2132b75605f96e0219099fa994b05d7db61
Ana Maria <ana.granada1@udea.edu.com>
2022-01-02 23:10:33 -0500

    fix: add translations

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
 
             <!-- Email Address -->
             <div class="mb-7">
-                <x-label for="email" :value="trans('users.info.email')" />
+                <x-label for="email" :value="trans('register.email')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
