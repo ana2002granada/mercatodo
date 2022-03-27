85860eb5cb008329880b7a2575ca94323a238df5
Ana Maria <ana.granada1@udea.edu.com>
2022-03-27 12:06:47 -0500

    feat: resolve pending transactions

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ResolvedPaymentTransactionCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -9,8 +10,8 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command(ResolvedPaymentTransactionCommand::class)->everyFiveMinutes();
     }
-
 
     protected function commands(): void
     {
