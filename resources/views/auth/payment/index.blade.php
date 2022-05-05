2ecc9363809494228dc2d340b9e46edd73adb5d7
Ana Maria <ana.granada1@udea.edu.com>
2022-05-04 21:32:00 -0500

    style: add translations

--- resources/views/auth/payment/index.blade.php
+++ resources/views/auth/payment/index.blade.php
@@ -3,7 +3,7 @@
         <div class="flex items-center flex-row mx-10 -mt-40">
             <div class="w-full max-h-screen h-full overflow-y-auto">
                 <div class="px-8 mt-24 py-14 bg-white">
-                    <p class="lg:text-4xl text-3xl font-black leading-10 text-gray-800 dark:text-white pt-3">Bag</p>
+                    <p class="lg:text-4xl text-3xl font-black leading-10 text-gray-800 dark:text-white pt-3"> {{ trans('payments.bag') }} </p>
                     @foreach($payment->products as $product)
                         <div class="py-8 py-8 border-t flex border-gray-50 hover:bg-gray-200 rounded-md p-4">
                         <div class="md:w-4/12 2xl:w-1/4 w-full">
@@ -14,7 +14,7 @@
                             <div class="flex items-center justify-between w-full pt-1">
                                 <div class="font-black w-full flex-grow leading-none text-gray-800 dark:text-white">{{ $product->name }}</div>
                                 <div class="flex items-center text-white justify-center gap-3 flex-grow bg-red-500 rounded-xl px-4">
-                                    <p class="font-bold text-sm">Count: </p>
+                                    <p class="font-bold text-sm"> {{trans('payments.count')}}: </p>
                                     <p class="text-xs leading-3">{{ $product->pivot->count }}</p>
                                 </div>
                             </div>
@@ -30,15 +30,15 @@
             </div>
             <div class="w-96 bg-black bg-opacity-90 h-screen p-5 flex flex-col justify-between pb-10">
                 <div class="flex flex-col h-auto px-8 py-20 justify-between mt-20">
-                    <p class="lg:text-4xl text-3xl font-black leading-9 text-gray-200">Summary</p>
+                    <p class="lg:text-4xl text-3xl font-black leading-9 text-gray-200">{{ trans('payments.purchase') }}</p>
                 </div>
                 <div>
                     <div class="flex items-center pb-6 justify-between lg:pt-5 pt-20 text-gray-200">
-                        <p class="text-2xl leading-normal">Total</p>
+                        <p class="text-2xl leading-normal">{{ trans('payments.total') }}</p>
                         <p class="text-2xl font-bold leading-normal text-right">{{ $payment->amount_format }}</p>
                     </div>
                     <button class="text-base leading-none w-full py-5 bg-gray-800 border-gray-800 border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white dark:hover:bg-gray-700" @click="$root.$emit('open-modal', {'route': '{{ route('payment.process', $payment) }}'})">
-                        Continuous
+                        {{ trans('payments.continuous') }}
                     </button>
                 </div>
             </div>
